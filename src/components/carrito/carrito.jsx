c343fca76efeac7877f8380b26ea38e44fd1fd21
Bautista Cola <i.cola@alumno.etec.um.edu.ar>
2023-09-14 21:42:19 -0300

    remove useless code

--- src/components/carrito/carrito.jsx
+++ src/components/carrito/carrito.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./carrito.css";
-import {Link} from 'react-router-dom';
 import { cartData as cartDataSingleton } from '../../cart_data'
 import { Product } from "../../models/product";
 
