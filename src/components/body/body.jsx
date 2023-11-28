5ade12c0994ca3a984f1ba01245240c2d19a016e
Bautista Cola <i.cola@alumno.etec.um.edu.ar>
2023-11-28 00:11:07 -0300

    productos desde back

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -1,19 +1,14 @@
 import "./body.css";
-import Air from "../../multimedia/Air.jpg";
-import Inspire from "../../multimedia/Inspire.jpg";
-import Spark from "../../multimedia/spark.jpg";
-import Mini2 from "../../multimedia/Mini2.jpg";
-import Air1 from "../../multimedia/Air1.jpg";
-import { Product } from "../../models/product";
-import { cartData as cartDataSingleton } from "../../cart_data";
+
+
 import { useContext, useEffect, useState } from "react";
 import { dataContext } from "../Context/DataContext";
-import "./body.css";
+
 
 import axios from "axios";
 
 export default function Body() {
-    
+   
 
     const [data1, setData] = useState([]);
 
@@ -34,14 +29,14 @@ export default function Body() {
     console.log(data1);
     console.log (data1["products"])
     return (
-        <div id='bodyMoviles'>
+        <div id='container-items'>
           <div className="body-items">
             <div className="container-items">
               {data1 && data1["products"] ? (
                 data1["products"].map((product) => (
                   <div className="item" key={product.id}>
                     <figure>
-                      <img src={product.img} alt="img-product-card"/>
+                      <img src={product.img} alt="item"/>
                     </figure>
                     <div className="info-product">
                       <h3>{product.name}</h3>
